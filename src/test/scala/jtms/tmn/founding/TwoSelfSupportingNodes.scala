53781abfedc94b49270c2708e02b30e3e54ab545
hbeck <harald.beck@gmx.at>
2016-03-12 15:12:54 +0100

    fixed test case two self supporting nodes

--- src/test/scala/jtms/tmn/founding/TwoSelfSupportingNodes.scala
+++ src/test/scala/jtms/tmn/founding/TwoSelfSupportingNodes.scala
@@ -2,7 +2,7 @@ package jtms.tmn.founding
 
 //import core.{Fact, Program, Rule, Atom}
 import core.{Program, Rule, Atom}
-import jtms.TMN
+import jtms.{out, TMN}
 import org.scalatest.FlatSpec
 
 /**
@@ -12,19 +12,37 @@ class TwoSelfSupportingNodes extends FlatSpec {
   val a = Atom("a")
   val b = Atom("b")
 
-  val r1 = Rule.pos(a).head(b)
-  val r2 = Rule.pos(b).head(a)
+  val r1 = Rule(b,a)
+  val r2 = Rule(a,b)
 
   val program = Program(r1, r2)
 
-  val tmn = TMN(program)
+  //stepwise:
 
+  val tmn0 = TMN()
+
+  val addedR1: Option[Set[Atom]] = tmn0.add(r1)
+  assert(tmn0.status(a) == out)
+  assert(tmn0.status(b) == out)
+  assert(addedR1.get.isEmpty)
+  assert(tmn0.getModel==Set[Atom]())
+
+  val addedR2: Option[Set[Atom]] = tmn0.add(r2)
+  assert(tmn0.status(a) == out)
+  assert(tmn0.status(b) == out)
+  assert(addedR2.get.isEmpty)
+  assert(tmn0.getModel==Set[Atom]())
+
+  //all at once:
+
+  val tmn = TMN(program)
 
   "A program containing only two self supporting nodes" should "have no model" in {
     println(tmn.getModel)
-    assert(tmn.getModel == None)
+    assert(tmn.getModel == Set[Atom]())
   }
 
+  //TODO (hb): use isFounded on specific list
 //  it should "not mark the model a, b as founded" in {
 //    assert(tmn.isFounded(Set(a, b)) == false)
 //  }
