1aeb6546c96596230b7c9bbe3c93c0cf667433f3
Christian Folie <christian.folie@gmail.com>
2016-05-13 14:13:51 +0200

    changed implementation of PinToTimePoint

--- src/main/scala/engine/PinAspProgramToTimePoint.scala
+++ src/main/scala/engine/PinAspProgramToTimePoint.scala
@@ -7,26 +7,28 @@ import core.lars.{TimePoint, Time}
 /**
   * Created by FM on 13.05.16.
   */
+case class PinToTimePoint(timePoint: TimePoint) {
+  def apply(dataStream: Stream): Set[AspRule] = {
+    val nowAtT = apply(now)
+
+    val pinnedAtoms = dataStream flatMap (x => PinToTimePoint(x.time).atoms(x.atoms))
+
+    pinnedAtoms + nowAtT
+  }
+
+  def atoms(atoms: Set[Atom]): Set[AspRule] = {
+    atoms map (apply(_))
+  }
+
+  def apply(atom: Atom): AspRule = {
+    AspFact(atom(timePoint))
+  }
+}
+
 object PinAspProgramToTimePoint {
   def apply(program: AspProgram, dataStream: Stream, timePoint: TimePoint) = {
-    val nowAtT = atomAtT(timePoint, now)
-
-    val atoms = dataStream flatMap atomsAtT
-
-    AspProgramAtTimePoint(program, atoms + nowAtT, timePoint)
+    AspProgramAtTimePoint(program, PinToTimePoint(timePoint)(dataStream), timePoint)
   }
-
-  def atomsAtT(streamEntry: StreamEntry): Set[AspRule] = atomsAtT(streamEntry.time, streamEntry.atoms)
-
-  def atomsAtT(timePoint: TimePoint, atoms: Set[Atom]): Set[AspRule] = {
-    atoms map (atomAtT(timePoint, _))
-  }
-
-  def atomAtT(time: TimePoint, atom: Atom): AspRule = {
-    AspFact(atom(time.timePoint))
-  }
-
-
 }
 
 case class AspProgramAtTimePoint(baseProgram: AspProgram, pinnedAtoms: Set[AspRule], timePoint: TimePoint) extends AspProgram {
--- src/test/scala/engine/TransformAspToTimePointSpec.scala
+++ src/test/scala/engine/TransformAspToTimePointSpec.scala
@@ -19,32 +19,30 @@ class TransformAspToTimePointSpec extends FlatSpec {
   val t1: TimePoint = 1
   val t2: TimePoint = 2
 
-  "An empty AspProgram with no dataStream at t1" should "contain only 'now(t1).'" in {
-    val p = AspProgram()
+  "An dataStream at t1" should "contain only 'now(t1).'" in {
     val dataStream: Stream = Set()
 
-    val transformed = PinAspProgramToTimePoint(p, dataStream, t1)
-    transformed.rules should contain(AspFact(now(t1)))
-    transformed.timePoint should be(t1)
+    val transformed = PinToTimePoint(t1)(dataStream)
+    transformed should contain(AspFact(now(t1)))
   }
-  "An empty AspProgram with dataStream 't1 -> a' at t1" should "contain  'a(t1), now(t1).'" in {
+
+  "An dataStream 't1 -> a' at t1" should "contain  'a(t1), now(t1).'" in {
     val p = AspProgram()
     val dataStream: Stream = Set(StreamEntry(t1, Set(a)))
 
-    val transformed = PinAspProgramToTimePoint(p, dataStream, t1)
+    val transformed = PinToTimePoint(t1)(dataStream)
 
-    transformed.rules should contain allOf(AspFact(a(t1)), AspFact(now(t1)))
+    transformed should contain allOf(AspFact(a(t1)), AspFact(now(t1)))
   }
 
-  "An empty AspProgram with dataStream 't1 -> {a,c}, t2 -> b' at t2" should "contain  'a(t1)., b(t2). now(t2).'" in {
-    val p = AspProgram()
+  "A dataStream 't1 -> {a,c}, t2 -> b' at t2" should "contain  'a(t1)., b(t2). now(t2).'" in {
     val dataStream: Stream = Set(
       StreamEntry(t1, Set(a, c)),
       StreamEntry(t2, Set(b))
     )
 
-    val transformed = PinAspProgramToTimePoint(p, dataStream, t2)
+    val transformed = PinToTimePoint(t2)(dataStream)
 
-    transformed.rules should contain allOf(AspFact(a(t1)),AspFact(c(t1)), AspFact(b(t2)), AspFact(now(t2)))
+    transformed should contain allOf(AspFact(a(t1)), AspFact(c(t1)), AspFact(b(t2)), AspFact(now(t2)))
   }
 }
