c9b9b3b20fe75b87ae2006b15aa51f6415e212e0
Christian Folie <christian.folie@gmail.com>
2016-05-28 16:26:42 +0200

    testing all implementations

--- src/test/scala/engine/EngineStreamSpec.scala
+++ src/test/scala/engine/EngineStreamSpec.scala
@@ -5,7 +5,7 @@ import core.asp.AspProgram
 import core.lars.Program
 import engine.asp.EvaluationStrategy
 import engine.config.{AspEvaluationEngineConfiguration, BuildEngine}
-import fixtures.TimeTestFixtures
+import fixtures.{EvaluateProgramWithAllImplementations, TimeTestFixtures}
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
 import org.scalatest.OptionValues._
@@ -13,46 +13,53 @@ import org.scalatest.OptionValues._
 /**
   * Created by FM on 21.04.16.
   */
-class EngineStreamSpec extends FlatSpec with TimeTestFixtures {
+class EngineStreamSpec extends FlatSpec with TimeTestFixtures with EvaluateProgramWithAllImplementations {
 
   val program = Program.from(
     a <= b,
     b <= c not d
   )
 
-  def evaluationEngine: EvaluationEngine = BuildEngine.withProgram(program).useAsp().withTms().use().usePull().start()
+  def evaluate(evaluationEngine: => EvaluationEngine) {
 
-  "Adding atoms one after another at the same timepoint" should "lead to different evaluation results" in {
-    val engine = evaluationEngine
+    it should "lead to different evaluation results" in {
+      info("Adding atoms one after another at the same timepoint")
 
-    val atT1 = engine.append(t1) _
+      val engine = evaluationEngine
 
-    atT1(Seq(Atom("c")))
+      val atT1 = engine.append(t1) _
 
-    assume(Set(a, b, c) subsetOf engine.evaluate(t1).get.value)
+      atT1(Seq(Atom("c")))
 
-    atT1(Seq(Atom("d")))
+      assume(Set(a, b, c) subsetOf engine.evaluate(t1).get.value)
 
-    engine.evaluate(t1).get.value should contain allOf(c, d)
+      atT1(Seq(Atom("d")))
+
+      engine.evaluate(t1).get.value should contain allOf(c, d)
+    }
+
+    it should "not lead to a result at t3" in {
+      info("Adding one atom at t2")
+      val engine = evaluationEngine
+
+      engine.append(t2)(Atom("c"))
+
+      assume(Set(a, b, c).subsetOf(engine.evaluate(t2).get.value))
+
+      assert(engine.evaluate(t3).get.value.isEmpty)
+    }
+
+    it should "not lead to a result when evaluating at t1" in {
+      info("Adding one atom at t2")
+      val engine = evaluationEngine
+
+      engine.append(t2)(Atom("c"))
+
+      assume(Set(a, b, c) subsetOf engine.evaluate(t2).get.value)
+
+      assert(engine.evaluate(t1).get.value.isEmpty)
+    }
   }
 
-  "Adding one atom at t2" should "not lead to a result at t3" in {
-    val engine = evaluationEngine
-
-    engine.append(t2)(Atom("c"))
-
-    assume(Set(a, b, c).subsetOf(engine.evaluate(t2).get.value))
-
-    assert(engine.evaluate(t3).get.value.isEmpty)
-  }
-
-  it should "not lead to a result when evaluating at t1" in {
-    val engine = evaluationEngine
-
-    engine.append(t2)(Atom("c"))
-
-    assume(Set(a, b, c) subsetOf engine.evaluate(t2).get.value)
-
-    assert(engine.evaluate(t1).get.value.isEmpty)
-  }
+  "Engine Stream Specs" must behave like runInAllImplementations(program)(evaluate)
 }
