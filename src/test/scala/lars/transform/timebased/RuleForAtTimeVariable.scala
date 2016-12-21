6abd3499e1e85c87c4992bc24eee0031f10732d1
Christian Folie <christian.folie@outlook.com>
2016-12-21 09:22:20 +0100

    changed mapping of @_U window

--- src/test/scala/lars/transform/timebased/RuleForAtTimeVariable.scala
+++ src/test/scala/lars/transform/timebased/RuleForAtTimeVariable.scala
@@ -15,7 +15,7 @@ class RuleForAtTimeVariable extends TransformLarsSpec {
   val w_te_1_at_U_a = W(1, At(U), a)
 
   "The rule for w^1 at_1 a" should "return 3 rules" in {
-    DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U) should have size (3)
+    DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U) should have size (2)
   }
   it should "contain now(T) in all rules" in {
     forAll(DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U)) {
@@ -23,8 +23,10 @@ class RuleForAtTimeVariable extends TransformLarsSpec {
     }
   }
 
-  it should "return two rules with head reach_w_te_1_at_U_a" in {
-    forExactly(2, DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U)) { rule => rule.head.toString should startWith("reach_w_te_1_at_U_a") }
+  it should "return two rules with head w_1_at_1_a" in {
+    forExactly(2, DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U)) {
+      rule => rule.head.toString should startWith("w_te_1_at_U_a")
+    }
   }
   it should "return  reach_w_te_1_at_U_a(T + 1, T)" in {
     forExactly(1, DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U)) {
@@ -37,31 +39,23 @@ class RuleForAtTimeVariable extends TransformLarsSpec {
     }
   }
 
-  it should "have head w_1_at_1_a for one rule" in {
-    forExactly(1, DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U)) { rule => rule.head.toString should startWith("w_te_1_at_U_a") }
-  }
-  it should "have one head with Time-Variables U,T" in {
+  it should "have tow heads with Time-Variables T,T" in {
     forExactly(1, DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U)) {
-      rule => headArguments(rule.head) should contain inOrder(U, T)
+      rule => headArguments(rule.head) should contain inOrder(T - 1, T)
     }
   }
 
-  it should "contain a(U) for one element" in {
-    forExactly(1, DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U)) { rule => rule.body should contain(a(U)) }
-  }
-  it should "contain reach_w_te_1_at_U_a(U,T) for one element" in {
-    val reach = Atom("reach_w_te_1_at_U_a")(U)
-    forExactly(1, DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U)) { rule => rule.body should contain(reach(T)) }
+  it should "contain a(T) for one element" in {
+    forExactly(1, DefaultLarsToPinnedProgram.rulesForAtTimeVariable(w_te_1_at_U_a, U)) { rule => rule.body should contain(a(T)) }
   }
 
-  "The rule for w^2 at_U a" should "have rules with heads reach_w_te_2_at_U_a(T-2,T), reach_w_te_2_at_U_a(T-1, T), reach_w_te_2_at_U_a(T,T), reach_w_te_2_at_U_a(U,T)" in {
-    val reach = Atom("reach_w_te_2_at_U_a")
+  "The rule for w^2 at_U a" should "have rules with heads w_te_2_at_U_a(T-2,T), w_te_2_at_U_a(T-1, T), w_te_2_at_U_a(T,T), w_te_2_at_U_a(U,T)" in {
+    val windowAtom = Atom("w_te_2_at_U_a")
     val body = DefaultLarsToPinnedProgram.rulesForAtTimeVariable(WindowAtom(SlidingTimeWindow(2), At(U), a), U) map (_.head)
     body should contain allOf(
-      reach(T - 2)(T),
-      reach(T - 1)(T),
-      reach(T)(T),
-      Atom("w_te_2_at_U_a")(U)(T)
+      windowAtom(T - 2)(T),
+      windowAtom(T - 1)(T),
+      windowAtom(T)(T)
     )
   }
 
