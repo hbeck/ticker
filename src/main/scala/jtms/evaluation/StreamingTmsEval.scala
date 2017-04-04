6cfd0b32a06915cd467cb19cb88eab3857ef2fb0
hbeck <harald.beck@gmx.at>
2017-04-04 19:43:05 +0200

    abstracted from mmedia example towards streaming tms eval instance

--- src/main/scala/jtms/evaluation/StreamingTmsEval.scala
+++ src/main/scala/jtms/evaluation/StreamingTmsEval.scala
@@ -8,6 +8,7 @@ import core.{Atom, Predicate}
 import core.asp._
 import jtms._
 import jtms.algorithms.{JtmsDoyle, JtmsGreedy, JtmsLearn}
+import jtms.evaluation.instances.MMediaInstance
 import jtms.networks.OptimizedNetwork
 import runner.Load
 
@@ -17,7 +18,7 @@ import scala.io.Source
 /**
   * Created by hb on 4/4/17.
   */
-object MMediaEval {
+object StreamingTmsEval {
 
   val loader = Load(TimeUnit.SECONDS)
 
@@ -31,7 +32,7 @@ object MMediaEval {
     var iterations = 1
     var windowSize = 10
     var timePoints = 360
-    //var instanceNames = Seq("p1")
+    var instanceNames = Seq("mmedia")
     //var dir = "src/test/resources/ground-programs/"
     if (args.nonEmpty) {
       try {
@@ -40,26 +41,35 @@ object MMediaEval {
         iterations = Integer.parseInt(args(2))
         windowSize = Integer.parseInt(args(3))
         timePoints = Integer.parseInt(args(4))
-        //instanceNames = args.drop(5).toSeq
+        instanceNames = args.drop(5).toSeq
       } catch {
         case e: Exception => {
-          println("args: impl warmUps iterations windowSize timePoints")
-          println("eg: doyle 2 10 30 10000")
+          println("args: impl warmUps iterations windowSize timePoints inst1 inst2 ...")
+          println("eg: doyle 2 10 30 180 mmedia")
           System.exit(1)
         }
       }
     }
     println(f"impl: $impl, warmUps: $warmUps, iterations: $iterations, windowSize: $windowSize, timePoints: $timePoints")
-    run(impl, warmUps, iterations, windowSize, timePoints)
+    run(impl, warmUps, iterations, windowSize, timePoints, instanceNames)
   }
 
-  def run(impl: String, warmUps: Int, iterations: Int, windowSize: Int, timePoints: Int) {
-    val inst = MMediaInstance(windowSize, timePoints)
-    println("mmedia")
-    runImplementation(impl, warmUps, iterations, inst)
+  def run(impl: String, warmUps: Int, iterations: Int, windowSize: Int, timePoints: Int, instanceNames: Seq[String]) {
+    for (instanceName <- instanceNames) {
+      println("mmedia")
+      val inst = makeInstance(instanceName,windowSize,timePoints)
+      runImplementation(impl, warmUps, iterations, inst)
+    }
   }
 
-  def runImplementation(impl: String, warmUps: Int, iterations: Int, instance: MMediaInstance): Unit = {
+  def makeInstance(instanceName: String, windowSize: Int, timePoints: Int): StreamingTmsEvalInstance = {
+    instanceName match {
+      case "mmedia" => MMediaInstance(windowSize, timePoints)
+      case _ => println(f"unknown instance name $instanceName"); throw new RuntimeException
+    }
+  }
+
+  def runImplementation(impl: String, warmUps: Int, iterations: Int, instance: StreamingTmsEvalInstance): Unit = {
 
     var totalTime = 0L
     var totalRetractions = 0L
@@ -147,7 +157,7 @@ object MMediaEval {
   val _nrOfRemovedRules = "nrRemoveRule"
   val _nrOfRemovedFacts = "nrRemoveFact"
 
-  def runIteration(inst: MMediaInstance, tms: JtmsUpdateAlgorithm): Map[String, Long] = {
+  def runIteration(inst: StreamingTmsEvalInstance, tms: JtmsUpdateAlgorithm): Map[String, Long] = {
 
     var models = 0L
     var failures = 0L
@@ -282,149 +292,3 @@ object MMediaEval {
 
 }
 
-case class MMediaInstance(windowSize: Int, timePoints: Int) {
-
-  val done = Atom("done")
-  val lfu = Atom("lfu")
-  val lru = Atom("lru")
-  val fifo = Atom("fifo")
-  val random = Atom("random")
-
-  val alpha_at = Predicate("alpha_at")
-  val high_at = Predicate("high_at")
-  val mid_at = Predicate("mid_at")
-  val low_at = Predicate("low_at")
-  val rtm_at = Predicate("rtm_at")
-  val lt = Predicate("lt")
-  val leq = Predicate("leq")
-
-  val spoil_high = Atom("spoil_high")
-  val spoil_mid = Atom("spoil_mid")
-  val spoil_low = Atom("spoil_low")
-
-  val wrtm = Atom("wrtm")
-
-  /*
-done :- lfu.
-done :- lru.
-done :- fifo.
-random :- not done.
-   */
-  val E = Set[Atom]()
-
-  val staticRules: Seq[NormalRule] = {
-    var rules = Seq[NormalRule]()
-    rules = rules :+ rule(done,lfu) :+ rule(done,lru) :+ rule(done,fifo) :+ rule(random,E,Set(done))
-    for (i <- 0 to 30) {
-      for (j <- 0 to 30) {
-        if (i < j) {
-          rules = rules :+ fact(f"lt($i,$j)")
-        }
-        if (i <= j) {
-          rules = rules :+ fact(f"leq($i,$j)")
-        }
-      }
-    }
-    rules
-  }
-
-
-//  val rulesToAdd: Map[Int,Seq[NormalRule]] = {
-//    (0 to timePoints) map (t => (t,rulesToAddAt(t))) toMap
-//  }
-//  val rulesToRemove: Map[Int,Seq[NormalRule]] = {
-//    (0 to timePoints) map (t => (t,rulesToRemoveAt(t))) toMap
-//  }
-
-  def factsToAddAt(t: Int): Seq[NormalRule] = {
-    var rules = Seq[NormalRule]()
-    rules = rules :+ fact(alpha_at(alphaValueFor(t),t))
-    rules
-  }
-
-  def factsToRemoveAt(t: Int): Seq[NormalRule] = {
-    var rules = Seq[NormalRule]()
-    rules = rules :+ fact(alpha_at(alphaValueFor(t-windowSize-1),t-windowSize-1))
-    rules
-  }
-
-  def alphaValueFor(t: Int): Int = {
-    val q = t%180
-    var v = 0
-    if (0 <= q && q < 60) {
-      v = 5
-    } else if (q >= 60 && q < 120) {
-      v = 15
-    } else {
-      v = 25
-    }
-    v
-  }
-
-  def verifyModel(tms: JtmsUpdateAlgorithm, t: Int) = {
-    val model = tms.getModel().get
-    val q = t % 180
-    if (q >= 0 && q < windowSize) {
-      assert(model.contains(random))
-    } else if (q >= windowSize && q < 60) {
-      assert(model.contains(random)) //low, if also rtm holds
-    } else if (q >= 60 && q < 60 + windowSize) {
-      assert(model.contains(random))
-    } else if (q >= (60 + windowSize) && q < 120) {
-      assert(model.contains(lru)) //mid
-    } else if (q >= 120 && q < (120 + windowSize)) {
-      assert(model.contains(random))
-    } else if (q >= (120 + windowSize) && q < 180) {
-      assert(model.contains(lfu)) //high
-    }
-  }
-
-  def rulesToAddAt(t: Int) = immediatelyExpiringRulesFor(t) ++ rulesExpiringAfterWindow(t)
-  def rulesToRemoveAt(t: Int) = immediatelyExpiringRulesFor(t-1) ++ rulesExpiringAfterWindow(t - windowSize - 1)
-
-  def immediatelyExpiringRulesFor(t: Int): Seq[NormalRule] = {
-    var rules = Seq[NormalRule]()
-    rules = rules :+
-      rule(lfu,Set[Atom](high_at(t)),Set[Atom](spoil_high)) :+ //lfu :- high_at(N), not spoil_high.
-      rule(lru,mid_at(t),spoil_mid) :+ //lru :- mid_at(N), not spoil_mid.
-      rule(fifo, Set[Atom](low_at(t),wrtm), Set[Atom](spoil_low)) //fifo :- low_at(N), not spoil_low, wrtm.
-
-    rules
-  }
-
-  def rulesExpiringAfterWindow(t: Int): Seq[NormalRule] = {
-    var rules = Seq[NormalRule]()
-    for (v <- 0 to 30) {
-      rules = rules :+ rule(high_at(t),Set[Atom](alpha_at(v,t),leq(18,v)),E) //high_at(T) :- alpha_at(V,T), leq(18,V).
-      rules = rules :+ rule(mid_at(t),Set[Atom](alpha_at(v,t),leq(12,v), lt(v,18)),E) //mid_at(T) :- alpha_at(V,T), leq(12,V), lt(V,18).
-      rules = rules :+ rule(low_at(t),Set[Atom](alpha_at(v,t),lt(v,12)),E) //low_at(T) :- alpha_at(V,T), lt(V,12).
-
-    }
-    rules = rules :+
-      rule(spoil_high,mid_at(t-1)) :+ //spoil_high :- mid_at(N-1).
-      rule(spoil_high,low_at(t-1)) :+ //spoil_high :- low_at(N-1).
-      rule(spoil_mid,high_at(t-1)) :+ //spoil_mid :- high_at(N-1).
-      rule(spoil_mid,low_at(t-1)) :+ //spoil_mid :- low_at(N-1).
-      rule(spoil_low,high_at(t-1)) :+ //spoil_low :- high_at(N-1).
-      rule(spoil_low,mid_at(t-1)) :+ //spoil_low :- mid_at(N-1).
-      rule(wrtm,rtm_at(t)) //wrtm :- rtm_at(N)
-
-    rules
-  }
-
-  def rule(head: Atom, posBody: Set[Atom], negBody: Set[Atom]): NormalRule = {
-    UserDefinedAspRule[Atom](head, posBody, negBody)
-  }
-
-  def rule(head: Atom, posBody: Atom): NormalRule = {
-    UserDefinedAspRule[Atom](head, Set(posBody), Set())
-  }
-
-  def rule(head: Atom, posBody: Atom, negBody: Atom): NormalRule = {
-    UserDefinedAspRule[Atom](head, Set(posBody), Set(negBody))
-  }
-
-  def fact(head: Atom): NormalRule = UserDefinedAspFact[Atom](head)
-  def fact(head: String): NormalRule = UserDefinedAspFact[Atom](Atom(head))
-
-}
---
+++ src/main/scala/jtms/evaluation/StreamingTmsEvalInstance.scala
@@ -0,0 +1,40 @@
+package jtms.evaluation
+
+import core.Atom
+import core.asp.{NormalRule, UserDefinedAspFact, UserDefinedAspRule}
+import jtms.JtmsUpdateAlgorithm
+
+/**
+  * Created by hb on 04.04.17.
+  */
+trait StreamingTmsEvalInstance {
+
+  def timePoints: Int
+  def staticRules(): Seq[NormalRule]
+  def factsToAddAt(t: Int): Seq[NormalRule]
+  def rulesToAddAt(t: Int): Seq[NormalRule]
+  def rulesToRemoveAt(t: Int): Seq[NormalRule]
+  def factsToRemoveAt(t: Int): Seq[NormalRule]
+  def verifyModel(tms: JtmsUpdateAlgorithm, t: Int)
+
+
+  //helper methods
+
+  def rule(head: Atom, posBody: Set[Atom], negBody: Set[Atom]): NormalRule = {
+    UserDefinedAspRule[Atom](head, posBody, negBody)
+  }
+
+  def rule(head: Atom, posBody: Atom): NormalRule = {
+    UserDefinedAspRule[Atom](head, Set(posBody), Set())
+  }
+
+  def rule(head: Atom, posBody: Atom, negBody: Atom): NormalRule = {
+    UserDefinedAspRule[Atom](head, Set(posBody), Set(negBody))
+  }
+
+  def fact(head: Atom): NormalRule = UserDefinedAspFact[Atom](head)
+  def fact(head: String): NormalRule = UserDefinedAspFact[Atom](Atom(head))
+
+}
+
+
