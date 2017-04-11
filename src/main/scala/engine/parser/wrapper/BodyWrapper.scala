9584ff817fbff579b3339899c47c756e41482503
Edward Toth <eddie@fstph.at>
2017-04-11 21:55:19 +0200

    new testfile for LarsParser

--- src/main/scala/engine/parser/wrapper/BodyWrapper.scala
+++
@@ -1,8 +0,0 @@
-package engine.parser.wrapper
-
-import engine.parser.factory.BodyTrait
-
-/**
-  * Created by et on 22.03.17.
-  */
-case class BodyWrapper(list: List[BodyTrait])
---
+++ src/test/scala/engine/parser/LarsParserTest.scala
@@ -0,0 +1,52 @@
+package engine.parser
+
+import core.lars.LarsProgram
+import org.scalatest.FlatSpec
+
+/**
+  * Created by et on 11.04.17.
+  */
+class LarsParserTest extends FlatSpec {
+
+  "The simple one-rule program" should "be accepted" in {
+    val program: Option[LarsProgram] = LarsParser("a :- b.",false)
+    assert(program.isDefined)
+    println(program.get)
+  }
+
+  "The one-rule program with two body atoms" should "be accepted" in {
+    val program: Option[LarsProgram] = LarsParser("a :- b,c.",false)
+    assert(program.isDefined)
+    println(program.get.toString())
+  }
+
+  "Left-hand operations" should "be accepted" in {
+    val program: Option[LarsProgram] = LarsParser("a :- 3+5=8.",false)
+    assert(program.isDefined)
+    println(program.get.toString())
+  }
+
+  "Right-hand operations" should "be accepted" in {
+    val program: Option[LarsProgram] = LarsParser("a :- 8=3+5.",false)
+    assert(program.isDefined)
+    println(program.get.toString())
+  }
+
+  "Two sided operations" should "not be accepted" in {
+    val program: Option[LarsProgram] = LarsParser("a :- 10-2=3+5.",false)
+    assert(program.isEmpty)
+    println(program.get.toString())
+  }
+
+  "Left-hand operations with variables" should "be accepted" in {
+    val program: Option[LarsProgram] = LarsParser("a :- 3+5=A.",false)
+    assert(program.isDefined)
+    println(program.get.toString())
+  }
+
+  "Right-hand operations with variables" should "be accepted" in {
+    val program: Option[LarsProgram] = LarsParser("a :- A=3+5.",false)
+    assert(program.isDefined)
+    println(program.get.toString())
+  }
+}
