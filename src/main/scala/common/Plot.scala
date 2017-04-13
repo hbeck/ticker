5f0e85a07733c3868d74ecfbc0cef0c43fb4290a
Christian Folie <christian.folie@outlook.com>
2017-04-13 22:17:22 +0200

    more formatting of plots

--- src/main/scala/common/Plot.scala
+++ src/main/scala/common/Plot.scala
@@ -21,7 +21,14 @@ object Plot {
       Seq(
         ("greedy", Seq(("a", 1), ("b", 2), ("c", 1))),
         ("doyle", Seq(("a", 2), ("b", 3), ("c", 4))),
-        ("clingo", Seq(("a", 3), ("b", 1), ("c", 2)))
+        ("clingo", Seq(("a", 3), ("b", 1), ("c", 2))),
+        ("bar", Seq(("a", 3), ("b", 1), ("c", 2))),
+        ("foo", Seq(("a", 3), ("b", 1), ("c", 2))),
+        ("baz", Seq(("a", 3), ("b", 1), ("c", 2))),
+        ("blub", Seq(("a", 3), ("b", 1), ("c", 2))),
+        ("blib", Seq(("a", 3), ("b", 1), ("c", 2))),
+        ("blob", Seq(("a", 3), ("b", 1), ("c", 2)))
+
       ),
       Options("bar foo")
     )
@@ -33,10 +40,64 @@ object Plot {
   }
 }
 
-case class Options(yAxisTitle: Option[String] = None) {
+
+object Color extends Enumeration {
+  type Type = Value
+  val Black, Grey = Value
+}
+
+trait FillStyles
+
+case class Empty(customColor: Option[Color.Type] = None) extends FillStyles
+
+case class Solid(customColor: Option[Color.Type] = None) extends FillStyles
+
+case class Pattern(customColor: Option[Color.Type] = None) extends FillStyles
+
+case class Options(yAxisTitle: Option[String] = None,
+                   fillStyles: Seq[FillStyles] = Seq(
+                     Empty(Some(Color.Black)),
+                     Solid(Some(Color.Black)),
+                     Solid(Some(Color.Grey)),
+                     Pattern(Some(Color.Black)),
+                     Pattern(Some(Color.Grey)),
+                     Pattern(Some(Color.Black)),
+                     Pattern(Some(Color.Grey))
+                   )
+                  ) {
   def configure(chart: BarChart) = {
     yAxisTitle.foreach(chart.y.label = _)
+
+    var patternCounter = 1 // pattern 0 is just empty
+    def asPattern(series: BarSeries) = {
+      series.fillStyle = FillStyle.Pattern
+      series.pattern = Some(patternCounter)
+      patternCounter = patternCounter + 1
+      if (patternCounter == 3)
+        patternCounter = 4 // skip solid
+    }
+
+    chart.data.serieses.zipWithIndex.foreach { case (s, i) =>
+      if (fillStyles.length > i)
+        fillStyles(i) match {
+          case Empty(None) => s.fillStyle = FillStyle.Empty
+          case Empty(Some(color)) =>
+            s.fillStyle = FillStyle.Empty
+            s.color = convertColor(color)
+          case Solid(None) => s.fillStyle = FillStyle.Solid
+          case Solid(Some(color)) =>
+            s.fillStyle = FillStyle.Solid
+            s.color = convertColor(color)
+          case Pattern(None) => asPattern(s)
+          case Pattern(Some(color)) =>
+            asPattern(s)
+            s.color = convertColor(color)
+        }
+    }
   }
+
+  private def convertColor(color: Color.Type) = org.sameersingh.scalaplot.Style.Color.withName(color.toString)
+
 }
 
 case class Plot(outputDirectory: String, generateRandomFilename: Boolean = true) {
@@ -72,11 +133,9 @@ case class Plot(outputDirectory: String, generateRandomFilename: Boolean = true)
 
     val data = new BarData(i => "\"%s\"" format (axisCaptions(i)._1), series)
 
-    data.serieses.foreach { s =>
-      s.fillStyle = FillStyle.Pattern
-    }
     val chart = new BarChart(title, data)
     chart.showLegend = true
+        chart.pointSize = Some(4)
     chart.legendPosX = LegendPosX.Left
     chart.legendPosY = LegendPosY.Top
 
@@ -112,6 +171,12 @@ case class Plot(outputDirectory: String, generateRandomFilename: Boolean = true)
       plotter.png(files.directory, files.name)
 
       PlotResult(Source.fromFile(files.gplFileName).mkString, files.gplFileName, files.as("png"))
+    }
+
+    def pdf() = {
+      plotter.pdf(files.directory, files.name)
+
+      PlotResult(Source.fromFile(files.gplFileName).mkString, files.gplFileName, files.as("pdf"))
     }
 
     def latex() = {
