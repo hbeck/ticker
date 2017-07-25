7b9cc8bdc7e42942bc70d631409db112c0be3050
hbeck <harald.beck@gmx.at>
2017-07-25 14:58:53 +0200

    removing debug line

--- src/main/scala/core/grounding/Grounding.scala
+++ src/main/scala/core/grounding/Grounding.scala
@@ -12,9 +12,6 @@ import core.Atom
 object Grounding {
 
   def ensureRuleRelations(rule: NormalRule): Option[NormalRule] = {
-    if (rule.pos.map(_.predicate.caption).contains("geq")) {
-      val a = 0
-    }
     if (relationsHold(rule)) {
       Some(deleteAuxiliaryAtoms(rule))
     } else {
